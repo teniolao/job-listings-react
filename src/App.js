241af7dbe73e33547e175b262bf37afae7c0759d
teniolao <Tennybetty100@gmail.com>
2022-04-11 18:33:20 +0100

    static card component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Card />
     </>
   );
-   
 }
 
 export default App;
